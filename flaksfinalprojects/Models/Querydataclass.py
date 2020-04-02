77252163e5802af814e5b14ee07672f67bce79d8
uribracha <uriluli1@gmail.com>
2020-04-02 21:14:05 +0300

    form created

--- flaksfinalprojects/Models/Querydataclass.py
+++ flaksfinalprojects/Models/Querydataclass.py
@@ -1,19 +1,27 @@
 from datetime import datetime
-
+from flaksfinalprojects import app
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms import Form, BooleanField, PasswordField
-from wtforms import TextField, TextAreaField, SelectField, DateField
+from wtforms import TextField, TextAreaField, SelectField, DateField,SelectMultipleField
 from wtforms import validators, ValidationError
+import pandas as pd
+import numpy as np
+from os import path
+import sys
 
 from wtforms.validators import DataRequired
 class Querydataclass(FlaskForm):
-    first_country=StringField("first country for checking",[validators.required("please put country"),validators.Length(2,message="please put valid country")])
-    second_country=StringField("second country for checking")
-    whattotest=SelectField("what to test", choices=[("greenhouse","only greenhouse gas"),("pop","only population"),("pop and greenhouse","both population and greenhouse gas")],validators=[validators.required("please put country")])
-    yearstart=DateField('Start year', format='%m/%d/%Y')
-    yearEnd=DateField('End Year', format='%m/%d/%Y')
+    df = pd.read_csv(path.join(app.root_path,"static\\Data\\greenhouse gas.csv"))
+    choose_data = []
+    for index,country in enumerate(df["Country"].unique()):
+       choose_data.append((index,country))
     submit = SubmitField('Submit')
+choose_countries=SelectMultipleField("choose country",choices=choose_data,[validators.Required])
+startYear=IntegerField("start year",[validators.Required,validators.number_range(1970,2017)])
+endYear=IntegerField("end year",[validators.Required,validators.number_range(1970,2017)])
+mode=SelectField("what do you want to compare",choices=[(0,"population data only"),(1,"greenhouse data only"),(2,"ratio between greenhouse gas emission and population count "),(3,"ratio between population count and greenhouse gas emission  ")] [validators.Required])
+typeofgraph=SelectField("select graph type",choices=[(0,"bar"), (1,"hist"), (2,"box"), (3,"kde"), (4,"area"), (5,"scatter"), (6,"hexbin"),(7,"pie")] [validators.Required])
 
 
 
